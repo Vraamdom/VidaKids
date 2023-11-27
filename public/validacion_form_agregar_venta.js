04d64574e3aef5d7c41d1e159000abab98080204
ttsendo <sebas.czr@gmail.com>
2023-11-27 11:14:20 -0500

    todas las validaciones, falta ordenar titulos

--- public/validacion_form_agregar_venta.js
+++ public/validacion_form_agregar_venta.js
@@ -1,158 +1,145 @@
-const formulario_add_venta = document.getElementById('form_agregar_venta')
-const nombre_cliente_add_venta = document.getElementById('nombre_venta')
-const apellido_cliente_add_venta = document.getElementById('apellido_venta')
-const fecha_add_venta = document.getElementById('fecha_venta')
-const precio_tot_add_venta = document.getElementById('precio_total_venta')
+const formulario = document.getElementById('form_add_venta');
+const inputs = document.querySelectorAll('#form_add_venta input');
 
-formulario_add_venta.addEventListener('submit', e => {
+
+const expresiones = {
+    placa: /^[a-zA-Z]{3}\s\d{3}$/,
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+    fecha: /^(?:(?:(?:0?[1-9]|1\d|2[0-8])[/](?:0?[1-9]|1[0-2])|(?:29|30)[/](?:0?[13-9]|1[0-2])|31[/](?:0?[13578]|1[02]))[/](?:0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|29[/]0?2[/](?:\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00))$/,
+    contraseñaFuerte: /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/,
+    un_nombre: /^[A-Z\u00C0-\u00D6\u00D8-\u00DE][a-z\u00DF-\u00F6\u00F8-\u00FF ]*$/,    //valida que solo la primera letra se mayuscula y el resto min
+    texto: /^[A-Z\u00C0-\u00D6\u00D8-\u00DE][a-zA-Z\u00C0-\u00D6\u00D8-\u00DE ]*$/,
+    numero: /^\d+$/,
+    money: /^[0-9]{1,3}(,[0-9]{3}){0,2}$/,
+
+
+
+}
+
+
+const campos = {
+    nombre_cliente_venta: false,
+    apellido_cliente_venta: false,
+    fecha_venta: false,
+    precio_total_venta: false,
+
+}
+
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+
+
+        case "nombre_cliente_venta":
+            validarCampo(expresiones.un_nombre, e.target, 'nombre_cliente_venta');
+            break;
+        case "apellido_cliente_venta":
+            validarCampo(expresiones.un_nombre, e.target, 'apellido_cliente_venta');
+            break;
+        case "fecha_venta":
+            validarCampo(expresiones.fecha, e.target, 'fecha_venta');
+            break;
+
+        case "precio_total_venta":
+            validarCampo(expresiones.money, e.target, 'precio_total_venta');
+            break;
+       
+
+
+
+    }
+
+
+}
+
+
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos[campo] = true;
+    } else {
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos[campo] = false;
+    }
+}
+
+
+const validarPassword2 = () => {
+    const inputPassword1 = document.getElementById('pass2');
+    const inputPassword2 = document.getElementById('pass2');
+
+
+    if (inputPassword1.value !== inputPassword2.value) {
+        document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+        document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos['password'] = false;
+    } else {
+        document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+        document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos['password'] = true;
+    }
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (campos.nombre_cliente_venta && campos.apellido_cliente_venta && campos.fecha_venta && campos.precio_total_venta ) {
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Se ha guardado el resgistro",
+            showConfirmButton: false,
+            timer: 1500
+        });
+    } else {
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+    }
+});
+
+const boton_volver = document.getElementById('boton_volver')
+const volver_index = () => {
+    Swal.fire({
+        title: "Quieres salir del formulario?",
+        showDenyButton: true,
+        confirmButtonText: "Seguir",
+        denyButtonText: `Volver`
+    }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            Swal.fire("Seguiras en el fomrulario", "", "success");
+        } else if (result.isDenied) {
+            Swal.fire("Redireccionando...", "", "info");
+            location.href = 'lista_ventas'
+        }   
+    });
+}
+boton_volver.addEventListener('click', e => {
     e.preventDefault()
 
-
-
-
-    validateInputs()
+    volver_index()
 })
-
-
-
-
-
-
-
-
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-
-
-
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
-}
-
-
-
-
-const setSuccess = elemento => {
-    const inputControl = elemento.parentElement
-    const errorDisplay = inputControl.querySelector('.error')
-
-
-
-
-    errorDisplay.innerText = ""
-    inputControl.classList.add('success')
-    inputControl.classList.remove('error')
-}
-
-
-// VALIDA QUE EL FORMATO DEL CORREO ESTE BIEN(TEXTO @ TEXTO . TEXTO)
-const validarCorreo = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-const validarFecha = fecha => {
-    const re = /^(2023)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-    return re.test(fecha)
-  
-}
-
-
-// VALIDA QUE TENGA ALMENOS 8 CARACTERES
-const validarContraseña = pass => {
-    const re = /^.{8,}$/
-    return re.test(Date(pass))
-}
-
-
-// VALIDA QUE SOLO SEAN LETRAS(SE PUEDEN ESPACIOS Y LETRAS CON ACENTOS)
-const validarTexto = text => {
-    const re = /^[A-Za-zÁáÉéÍíÓóÚúÑñ\s]*$/
-    return re.test(String(text))
-}   
-
-
-// VALIDA QUE SOLO SE INGRESEN LETRAS Y QUE LA PRIMERA SEA MAYUSCULA (LA PUEDEN USAR PARA CAMPOS COMO NOMBRES)
-const validarNombre = nombre => {
-    const re = /^[A-Z\u00C0-\u00D6\u00D8-\u00DE][a-zA-Z\u00C0-\u00D6\u00D8-\u00DE ]*$/
-    return re.test(nombre)
-}
-
-const validarDireccion = cadena => {
-    const re = /^[A-Za-z\u00C0-\u00D6\u00D8-\u00DE0-9\s#-]+$/
-    return re.test(cadena)
-  }
-  
-
-// VALIDA QUE SOLO SE PUEDAN INGRESAR NUMEROS
-const validarNumero = numero => {
-    const re = /^\d+$/
-    return re.test(numero)
-}
-
-// TRAE LA FECHA Y HORA ACTUAL AL CAMPO DATE TIME
-// const fechaHoraActual = new Date().toISOString().slice(0, 16);
-// datetime.value = fechaHoraActual
-
-
-const validateInputs = () => {
-    const nombreValueVenta = nombre_cliente_add_venta.value.trim()
-    const ApellidoValueVenta = apellido_cliente_add_venta.value.trim()
-    const FechaValue = fecha_add_venta.value.trim()
-    const totalValue = precio_tot_add_venta.value.trim()
-
-
-
-    if (ApellidoValueVenta === "") {
-        setError(apellido_cliente_add_venta, 'No puedes dejar este campo vacio.')
-    } else if (!validarNombre(ApellidoValueVenta)) {
-        setError(apellido_cliente_add_venta, 'Se debe iniciar con la letra mayuscula y no se permiten caracteres especiales.')
-    } else {
-        setSuccess(apellido_cliente_add_venta)
-    }
-
-
-    if (nombreValueVenta === "") {
-        setError(nombre_cliente_add_venta, 'No puedes dejar este campo vacio.')
-    } else if (!validarNombre(nombreValueVenta)) {
-        setError(nombre_cliente_add_venta, 'Se debe iniciar con la letra mayuscula y no se permiten caracteres especiales.')
-    } else {
-        setSuccess(nombre_cliente_add_venta)
-    }
-
-    if (FechaValue == "") {
-        setError(fecha_add_venta,'No se puede dejar el campo vacio')
-    }else if (!validarFecha(FechaValue)) {
-        setError(fecha_add_venta, 'debe ser con fecha y hora actuales')
-    }else{
-        setSuccess(fecha_add_venta)
-    }
-
-
-    if (totalValue === "") {
-        setError(precio_tot_add_venta, 'No puedes dejar este campo vacio.')
-    } else if (!validarNumero(totalValue)) {
-        setError(precio_tot_add_venta, 'Solo puedes ingresar números.')
-    } else {
-        setSuccess(precio_tot_add_venta)
-    }
-
-
-}
-
-
-// if (nombre_add_cliente && password_add_cliente && email_add_cliente && telefono_add_cliente && pais_add_cliente && departamento_add_cliente && direccion_add_cliente ) {
-    
-// function mostrarAlertaSucces() {
-//     Swal.fire({
-//         position: "center",
-//         icon: "success",
-//         title: "Your work has been saved",
-//         showConfirmButton: false,
-//         timer: 1500
-//     });
-// }  
-
