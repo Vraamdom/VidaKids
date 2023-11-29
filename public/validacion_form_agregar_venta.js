f0048131d9c9e5b709908c8820518e9f56f13090
ttsendo <sebas.czr@gmail.com>
2023-11-29 11:02:29 -0500

    SELECT SE AGREGA A EL AGREGAR VENTA; CON LOS PRODU

--- public/validacion_form_agregar_venta.js
+++ public/validacion_form_agregar_venta.js
@@ -110,7 +110,7 @@ inputs.forEach((input) => {
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if (campos.nombre_cliente_venta && campos.apellido_cliente_venta && campos.precio_total_venta && campos.select_estado_venta.value != "seleccione") {
+    if (campos.nombre_cliente_venta && campos.apellido_cliente_venta ) {
         Swal.fire({
             position: "center",
             icon: "success",
